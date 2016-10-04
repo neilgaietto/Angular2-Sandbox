fbb1a8c39b924011347c7072dfb28b0a13e21c1e
NeilGaietto <neil.gaietto@gmail.com>
2016-10-03 20:37:28 -0400

    project init

---
+++ Angular2-Sandbox/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Angular2_Sandbox.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: About
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
